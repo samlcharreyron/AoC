57e1c08412338bcf52b08884677504aec05e5692
Samuel <samlcharreyron@gmail.com>
2019-12-06 10:34:29 +0100

    refactor

--- 2019/6/main.py
+++ 2019/6/main.py
@@ -1,13 +1,6 @@
 import networkx as nx
 from collections import defaultdict
 
-def get_orbits(lines):
-    g = defaultdict(list)
-    for line in lines:
-        center, orbiter = line.split(')')[0:2]
-        g[orbiter].append(center)
-    return g
-
 def get_orbit_graph(lines):
     G = nx.Graph()
     for line in lines:
@@ -18,10 +11,10 @@ def get_orbit_graph(lines):
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()
-        g = get_orbit_graph(lines)
-        print(sum(v for v in nx.single_source_shortest_path_length(g, 'COM').values()))
+    g = get_orbit_graph(lines)
+    print(sum(v for v in nx.single_source_shortest_path_length(g, 'COM').values()))
 
-        start = g.neighbors('YOU').next()
-        end = g.neighbors('SAN').next()
-        print(nx.shortest_path_length(g, start, end))
+    start = g.neighbors('YOU').next()
+    end = g.neighbors('SAN').next()
+    print(nx.shortest_path_length(g, start, end))
 
