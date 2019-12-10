ff96bcd387fe7922a6b159ca280cb52107bdecef
Samuel <samlcharreyron@gmail.com>
2019-12-10 13:03:56 +0100

    refactor

--- 2019/10/main.py
+++ 2019/10/main.py
@@ -1,33 +1,28 @@
 import numpy as np
 import itertools
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 if __name__ == '__main__':
     data = np.genfromtxt('input.txt', comments='%', dtype=np.character, delimiter=1)
-
     yv, xv = np.nonzero(data == '#')
+
     pv = np.stack((xv, yv), axis=1)
     indices = range(len(xv))
     num_ast = np.zeros_like(xv, dtype=int)
 
     for i in indices:
         rays = pv[list(set(indices) - {i}), :] - pv[i,:]
-        lengths = np.linalg.norm(rays, axis=1)
-        rays_n = rays / np.tile(lengths, (2,1)).T
-        rays_n = rays_n.round(4)
-        num_ast[i] = len(np.unique(rays_n, axis=0))
+        angles = np.mod(np.arctan2(rays[:,1], rays[:,0]) + np.pi/2 + 2*np.pi, 2*np.pi)
+        num_ast[i] = len(np.unique(angles, axis=0))
     print(max(num_ast))
 
     # part 2
     im = np.argmax(num_ast)
-    print(pv[im,:])
 
     others = list(set(indices) - {im})
     rays = pv[others, :] - pv[im, :]
     lengths = np.linalg.norm(rays, axis=1)
-    rays_n = rays / np.tile(lengths, (2,1)).T
-    rays_n = rays_n.round(4)
-    angles = np.mod(np.arctan2(rays_n[:,1], rays_n[:,0]) + np.pi/2 + 2*np.pi, 2*np.pi)
+    angles = np.mod(np.arctan2(rays[:,1], rays[:,0]) + np.pi/2 + 2*np.pi, 2*np.pi)
     radar = defaultdict(list)
     for angle, other in zip(angles, others):
         radar[angle].append(pv[other,:])
@@ -42,6 +37,6 @@ if __name__ == '__main__':
                 n += 1
                 #print('{}: removed {}'.format(n, p))
                 if n == 200:
-                    print('removing {} with code {}'.format(p, 100*p[0]+p[1]))
+                    print('200: removing {} with code {}'.format(p, 100*p[0]+p[1]))
         i += 1
 
