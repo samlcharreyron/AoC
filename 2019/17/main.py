c69867f67f3acebe39449d97644d90478d51fa7c
Samuel <samlcharreyron@gmail.com>
2019-12-18 00:43:20 +0100

    trying part 2 day 17

--- 2019/17/main.py
+++ 2019/17/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy.signal
-import cv2
-import matplotlib.pyplot as plt
+import networkx as nx
+from scipy.spatial import cKDTree
+#import matplotlib.pyplot as plt
 
 class Program(object):
 
@@ -164,6 +165,26 @@ def conv(x):
     else:
         raise ValueError
 
+def get_path_code(path):
+    direction = complex(0,-1)
+    prev = path[0]
+    code = []
+    for new in path[1:]:
+        dp = new - prev
+        prev = new
+        if direction != dp:
+            # what about case where need to rotate 180
+            if int((dp / direction).imag) == 1:
+                code.append('R')
+                direction *= complex(0,1)
+            else:
+                code.append('L')
+                direction *= complex(0,-1)
+            code.append(1)
+        else:
+            code[-1] += 1
+    return code
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         line = f.read().splitlines()[0]
@@ -174,10 +195,30 @@ if __name__ == '__main__':
     g = program.run(-1)
     outputs = ''.join(chr(output) for output in g)
     #print(outputs)
+    # find starting post
+    m = np.array([[c for c in line] for line in outputs.splitlines()[:-1]])
+    yv, xv = np.where(m=='^')
+    start = complex(xv[0], yv[0])
+    orientation = complex(0, 1)
     m = np.array([[conv(c) for c in line] for line in outputs.splitlines()[:-1]])
     corner = np.array([[0,1,0],[1,1,1], [0,1,0]], np.int)
     mask = scipy.signal.convolve2d(m, corner)
-    xv,yv = np.where(mask == 5)
-    print(xv, yv)
+    yv,xv = np.where(mask == 5)
     print(sum((x-1) * (y-1) for x, y in zip(xv, yv)))
+
+    G = nx.Graph()
+    yv, xv = np.where(m==1)
+    tree = cKDTree(np.stack((xv, yv), axis=1))
+
+    for i,j in tree.query_pairs(r=1):
+        G.add_edge(complex(xv[i], yv[i]), complex(xv[j], yv[j]))
+    # find end
+    end = set([k for k, v in nx.degree(G) if v < 2]).difference(set([start])).pop()
     
+    print([len(path) for path in nx.node_disjoint_paths(G, start, end)])
+    long_path = max((path for path in nx.all_simple_paths(G, start, end)),
+                    key=lambda path: len(path))
+    code = get_path_code(long_path)
+    print(code)
+
+
