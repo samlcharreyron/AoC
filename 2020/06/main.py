6b346d7455bc35ffc14c418bd376de103cf6335d
Samuel Charreyron <samlcharreyron@gmail.com>
2020-12-06 14:01:25 +0100

    done day 6

--- 2020/06/main.py
+++ 2020/06/main.py
@@ -1,5 +1,15 @@
+from functools import reduce
+
+def p1(entries):
+    return sum(len(set(''.join(g))) for g in entries)
+
+def p2(entries):
+    return sum(len(reduce(set.intersection, (set(''.join(p)) for p in g))) 
+            for g in entries)
+    
+
 if __name__ == '__main__':
-    with open('test.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         lines = f.readlines()
         entries = [[]]
         i = 0
@@ -10,4 +20,5 @@ if __name__ == '__main__':
                 i += 1
             else:
                 entries[i].append(x)
-        print(entries)
+        print(p1(entries))
+        print(p2(entries))
