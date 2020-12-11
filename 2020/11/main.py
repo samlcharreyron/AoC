43408d4500ddf5aa17ec6e16485eedc806bf6ffc
Samuel Charreyron <samlcharreyron@gmail.com>
2020-12-11 18:27:45 +0100

    refactor

--- 2020/11/main.py
+++ 2020/11/main.py
@@ -1,4 +1,6 @@
 from math import sqrt, ceil
+from collections import defaultdict
+import time
 
 def p1(m):
     adj = (complex(-1,0), complex(1, 0), complex(0, -1), complex(0, 1),
@@ -26,26 +28,30 @@ def p2(m):
 
     md = 2 * ceil(sqrt(pow(max(k.real for k in m.keys()), 2) + pow(max(k.imag for k in m.keys()), 2)))
 
-    nc = 1
-    while nc != 0:
-        m_ = m.copy()
-        nc = 0
-        #import pdb; pdb.set_trace()
-        for k in m.keys():
-            ns = []
-            for d in dr:
-                for j in range(1, md):
-                    if (k + j * d) in m.keys():
-                        ns.append(k + j * d)
-                        break
+    # find neighbors
+    nb = defaultdict(list)
+    for k in m.keys():
+        for d in dr:
+            for j in range(1, md):
+                if (k + j * d) in m.keys():
+                    nb[k].append(k+j*d)
+                    break
 
-            if m[k] == 'L' and not any(m[n] == '#' for n in ns):
-                m_[k] = '#'
-                nc += 1
-            elif m[k] == '#' and (sum(m[n] == '#' for n in ns) > 4):
-                m_[k] = 'L'
-                nc += 1
-        m = m_
+    changed = set(m.keys())
+    changes = ['d']
+    while changes:
+        changes = []
+
+        for k in m.keys() & changed:
+            if m[k] == 'L' and not any(m[n] == '#' for n in nb[k]):
+                changes.append((k, '#'))
+            elif m[k] == '#' and (sum(m[n] == '#' for n in nb[k]) > 4):
+                changes.append((k, 'L'))
+
+        for k, v in changes:
+            m[k] = v
+
+        changed = set(k for k,_ in changes)
 
     return sum(v == '#' for v in m.values())
 
@@ -59,5 +65,8 @@ if __name__ == '__main__':
                     m[complex(j, i)] = c
 
         #print(p1(m.copy()))
+        tick = time.time()
         print(p2(m.copy()))
+        tock = time.time()
+        print('elapsed: ', tock - tick, ' s')
 
