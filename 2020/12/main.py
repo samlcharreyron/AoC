87e5164c68236df1000677cb8ca80f7e75dfae88
Samuel Charreyron <samlcharreyron@gmail.com>
2020-12-12 13:19:57 +0100

    day 12 p2

--- 2020/12/main.py
+++ 2020/12/main.py
@@ -1,5 +1,4 @@
-import math
-import cmath
+from math import pi, radians, cos, sin, sqrt
 
 def p1(data):
     drs = {       
@@ -7,6 +6,16 @@ def p1(data):
             'W': complex(-1, 0),
             'N': complex(0, 1),
             'S': complex(0, -1)
+            }
+
+    ans = {
+            -270 : complex(0, 1),
+            -180: complex(-1, 0),
+            -90 : complex(0, -1),
+            0 : complex(1, 0),
+            90 : complex(0, 1),
+            180: complex(-1, 0),
+            270: complex(0, -1),
             }
 
     d = drs['E']
@@ -18,18 +27,54 @@ def p1(data):
         if i in drs.keys():
             p += n * drs[i]
         elif i  == 'L':
-            d *= cmath.rect(1, n * math.pi/180)
+            d *= ans[n]
         elif i == 'R':
-            d *= cmath.rect(1, -n * math.pi/180)
+            d *= ans[-n]
         elif i == 'F':
             p += n * d
-    return abs(p.real) + abs(p.imag)
+    return round(abs(p.real) + abs(p.imag))
+
+def p2(data):
+    drs = {       
+            'E': complex(1, 0),
+            'W': complex(-1, 0),
+            'N': complex(0, 1),
+            'S': complex(0, -1)
+            }
+
+    ans = {
+            -270 : complex(0, 1),
+            -180: complex(-1, 0),
+            -90 : complex(0, -1),
+            0 : complex(1, 0),
+            90 : complex(0, 1),
+            180: complex(-1, 0),
+            270: complex(0, -1),
+            }
+
+    wp = complex(10, 1)
+    sp = complex(0, 0)
+
+    for line in data:
+        i = line[0]
+        n = int(line[1:])
+        if i in drs.keys():
+            wp += n * drs[i]
+        elif i  == 'L':
+            wp *= ans[n]
+        elif i == 'R':
+            wp *= ans[-n]
+        elif i == 'F':
+            sp += n * wp 
+
+    return round(abs(sp.real) + abs(sp.imag))
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         data = f.read().splitlines()
 
     print(p1(data))
+    print(p2(data))
 
 
 
