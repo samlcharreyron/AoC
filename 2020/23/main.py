b7841a910db4cca5378540f3274e275ebeb9a7b0
Samuel Charreyron <samlcharreyron@gmail.com>
2020-12-23 19:33:56 +0100

    part 2

--- 2020/23/main.py
+++ 2020/23/main.py
@@ -1,43 +1,58 @@
-from collections import deque
-from bisect import bisect_left
+
+def play(data, is_p2=False):
+    q = [int(c) for c in data]
+
+    if is_p2:
+        q.extend(range(max(q)+1,1000001))
+
+    qs = sorted(q)
+
+    ll = dict()
+    for c, cn in zip(q, q[1:]):
+        ll[c] = cn
+
+    ll[q[-1]] = q[0]
+
+    if is_p2:
+        ur = 10000001
+    else:
+        ur = 101
+
+    c = q[0]
+
+    for i in range(1, ur):
+        c1 = ll[c]
+        c2 = ll[c1]
+        c3 = ll[c2]
+
+        ll[c] = ll[c3]
+
+        di = c - 2
+        d = qs[di]
+
+        while d in (c1, c2, c3):
+            di -= 1
+            d = qs[di]
+
+        ll[c3] = ll[d]
+        ll[d] = c1
+        #print(d, c1, c2, c3, ll)
+
+        c = ll[c]
+
+    if is_p2:
+        d = ll[1]
+        return d * ll[d]
+    else:
+        c = 1
+        q = []
+        for _ in range(len(ll)-1):
+            c = ll[c]
+            q.append(c)
+        return ''.join(str(c) for c in q)
 
 if __name__ == '__main__':
     #data = '389125467'
     data = '624397158'
-
-    q = deque(int(c) for c in data)
-
-    for i in range(1, 101):
-        q.rotate(-1)
-        c1 = q.popleft()
-        c2 = q.popleft()
-        c3 = q.popleft()
-        q.rotate(1)
-        c = q[0]
-        qs = sorted(list(q))
-        didxs = bisect_left(qs, c - 1)
-        #import pdb; pdb.set_trace()
-
-        if didxs == 0: 
-            if qs[0] != (c-1):
-                d = qs[-1]
-            else:
-                d = qs[0]
-        else:
-            if qs[didxs] == (c-1):
-                d = c - 1
-            else:
-                d = qs[didxs - 1]
-
-        didx = q.index(d)
-        q.rotate((-didx - 1))
-        q.extendleft([c3, c2, c1])
-        q.rotate(didx + 1)
-            
-        q.rotate(-1)
-        #print(i, d, q)
-
-    idx = q.index(1)
-    q.rotate(-idx)
-    q.popleft()
-    print(''.join(str(c) for c in q))
+    print(play(data))
+    print(play(data, is_p2=True))
