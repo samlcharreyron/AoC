4d40a049913b663220e3dbd566cd764251267a54
Samuel Charreyron <samlcharreyron@gmail.com>
2020-12-24 11:30:04 +0100

    p2

--- 2020/24/main.py
+++ 2020/24/main.py
@@ -25,25 +25,58 @@ def gen_dirs(line):
                 i+= 1
                 yield c1
 
-if __name__ == '__main__':
+def p1(lines):
     grid = defaultdict(bool)
-    flipped = defaultdict(int)
 
+    for line in lines:
+        p = (0, 0, 0)
+        for m in gen_dirs(line):
+            p = tuple(a + b for a, b in zip(p, dirs[m]))
+
+        grid[p] = not grid[p]
+
+    return sum(grid.values())
+
+def p2(lines):
+    grid = defaultdict(bool)
+
+    for line in lines:
+        p = (0, 0, 0)
+        for m in gen_dirs(line):
+            p = tuple(a + b for a, b in zip(p, dirs[m]))
+
+        grid[p] = not grid[p]
+
+    for i in range(1, 101):
+        to_flip = set()
+        to_visit = set(grid.keys())
+        for p in to_visit.copy():
+            nbrs = set(tuple(a+b for a, b in zip(p, dp)) for dp in dirs.values())
+            to_visit |= nbrs
+
+        for p in to_visit:
+            nbrs = [tuple(a+b for a, b in zip(p, dp)) for dp in dirs.values()]
+            if grid[p]:
+                num_b = sum(grid[nb] for nb in nbrs)
+                if num_b == 0 or num_b > 2:
+                    to_flip.add(p)
+            else:
+                num_b = sum(grid[nb] for nb in nbrs)
+                if num_b == 2:
+                    to_flip.add(p)
+
+        for p in to_flip:
+            grid[p] = not grid[p]
+
+        print('Day ', i, sum(v for v in grid.values()))
+
+    return(sum(v for v in grid.values()))
+            
+
+if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()
-        
-        for line in lines:
-            p = (0, 0, 0)
-            for m in gen_dirs(line):
-                p = tuple(a + b for a, b in zip(p, dirs[m]))
 
-            grid[p] = not grid[p]
-            flipped[p] += 1
-
-        #print(grid)
-        #print(sum(v == 1 for v in flipped.values()))
-        #print(sum(v == 2 for v in flipped.values()))
-        #print(sum(v == 3 for v in flipped.values()))
-        print(sum(grid.values()))
-
+    print(p1(lines))
+    print(p2(lines))
 
