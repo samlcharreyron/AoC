8bff0d1493436bf17aa7532091691ff003c6da77
Samuel Charreyron <samlcharreyron@gmail.com>
2021-12-27 12:45:07 +0000

    day 12 p2

--- 2021/12/main.cpp
+++ 2021/12/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <unordered_map>
+#include <map>
 #include <unordered_set>
 #include <numeric>
 
@@ -15,26 +16,41 @@ struct Node {
 };
 
 using Nodes = unordered_map<string, Node>;
+using Visited = map<string, int>;
 
-size_t dfs(const string name, const Nodes& nodes, const unordered_set<string>& visited) {
+bool is_lower(const string& s) {
+  return all_of(s.begin(), s.end(), [](const auto c) { 
+    return islower(c); });
+}
+
+size_t dfs(const string name, const Nodes& nodes, const Visited& visited, const int visits=1) {
 
   if (name == "end") {
+    //for (auto p : visited) {
+    //  cout << p.first << " (" << p.second << ") ";
+    //}
+    //cout << endl;
     return 1;
   }
 
-  if (visited.find(name) != visited.end()) {
+  bool already_full = any_of(visited.begin(), visited.end(), 
+    [visits](auto p) { return is_lower(p.first) && p.second == visits; });
+
+  auto it = visited.find(name);
+  if (name == "start" && it != visited.end() && it->second == 1) {
     return 0;
   }
 
-  unordered_set<string> visited_(visited);
-  if (all_of(name.begin(), name.end(), [](const auto c) { 
-    return islower(c); })) {
-    visited_.insert(name);
+  if (already_full && is_lower(name) && it != visited.end()) {
+    return 0;
   }
 
+  Visited visited_(visited);
+  visited_[name]++;
+
   return accumulate(nodes.at(name).children.begin(),
-    nodes.at(name).children.end(), 0, [&nodes, &visited_](const auto lhs, const auto pc) {
-    return lhs + dfs(pc->name, nodes, visited_);
+    nodes.at(name).children.end(), 0, [&nodes, &visited_, visits](const auto lhs, const auto pc) {
+    return lhs + dfs(pc->name, nodes, visited_, visits);
   });
 }
 
@@ -61,11 +77,40 @@ void part1() {
     nodes[to].children.push_back(&nodes[from]);
   }
 
-  unordered_set<string> visited;
-  cout << dfs("start", nodes, visited) << endl;
+  Visited visited;
+  cout << dfs("start", nodes, visited, 1) << endl;
+}
+
+void part2() {
+  Nodes nodes;
+  ifstream ifs("input.txt");
+
+  string line;
+  while(getline(ifs, line)) {
+    int dash = line.find('-');
+    string from = line.substr(0, dash);
+    string to = line.substr(dash+1);
+
+    if (nodes.find(from) == nodes.end()) {
+      nodes[from] = Node(from);
+    }
+
+    if (nodes.find(to) == nodes.end()) {
+      nodes[to] = Node(to);
+    }
+
+    nodes[from].children.push_back(&nodes[to]);
+    nodes[to].children.push_back(&nodes[from]);
+  }
+
+  Visited visited;
+  cout << dfs("start", nodes, visited, 2) << endl;
+
+  //cout << dfs("start", nodes, visited, 2) << endl;
 }
 
 int main() {
-  part1();
+  //part1();
+  part2();
   return 0;
 }
