e4e6942c40997f79cf5a8c856c1641bd8c2c7987
Samuel Charreyron <samlcharreyron@gmail.com>
2022-12-15 18:04:56 +0100

    Day 13 part 2

--- 2022/13/main.py
+++ 2022/13/main.py
@@ -5,29 +5,29 @@ def check(l, r):
     # import ipdb; ipdb.set_trace()
     if isinstance(l, int) and isinstance(r, int):
         if l < r:
-            return 1
-        elif l > r:
-            return 0 
-        else:
             return -1
+        elif l > r:
+            return 1 
+        else:
+            return 0 
     elif isinstance(l, list) and isinstance(r, list):
         for ll, rr in zip(l, r):
-            if check(ll, rr) == 1:
+            if check(ll, rr) == -1:
                 # print('l is smaller')
-                return 1
-            elif check(ll, rr) == 0:
+                return -1
+            elif check(ll, rr) == 1:
                 # print('r is smaller')
-                return 0
+                return 1
 
         if len(l) < len(r):
             # print('l ran out')
-            return 1
+            return -1
         elif len(r) < len(l):
             # print('r ran out')
-            return 0
+            return 1
         else:
             # print('equal')
-            return -1
+            return 0
 
     elif isinstance(l, list) and isinstance(r, int):
         return check(l, [r])
@@ -35,7 +35,8 @@ def check(l, r):
         return check([l], r)
 
 if __name__ == '__main__':
-    with open('input', 'r') as f:
+    filename = 'input'
+    with open(filename, 'r') as f:
         pairs = []
         for pair in f.read().split('\n\n'):
             left = ast.literal_eval(pair.splitlines()[0])
@@ -49,9 +50,9 @@ if __name__ == '__main__':
     #     print('right order: ', check(left, right))
     #     print('\n')
 
-    print(sum(i+1 for i, (left, right) in enumerate(pairs) if check(left, right) == 1))
+    print(sum(i+1 for i, (left, right) in enumerate(pairs) if check(left, right) == -1))
     
-    with open('test', 'r') as f:
+    with open(filename, 'r') as f:
         codes = []
         for pair in f.read().split('\n\n'):
             left = ast.literal_eval(pair.splitlines()[0])
@@ -59,6 +60,10 @@ if __name__ == '__main__':
             codes.append(left)
             codes.append(right)
 
+    codes.append([[2]])
+    codes.append([[6]])
     codes = sorted(codes, key=functools.cmp_to_key(check))
-    for code in codes:
-        print(code)
+    x, y = [i for i, code in enumerate(codes) if code == [[2]] or code == [[6]]]
+    print((x + 1) * (y + 1))
+    #for code in codes:
+    #    print(code)
